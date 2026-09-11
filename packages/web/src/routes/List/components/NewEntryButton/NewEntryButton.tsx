5248862e7a07a3e302422113f2b10c4ed6fa3c4e
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2026-09-11 10:13:15 +0200

    temp

--- packages/web/src/routes/List/components/NewEntryButton/NewEntryButton.tsx
+++ packages/web/src/routes/List/components/NewEntryButton/NewEntryButton.tsx
@@ -1,17 +1,28 @@
 import type { FC } from "react";
+import { join } from "../../../../utils/join";
 
 type Props = {
   pending: boolean;
   onClick: () => void;
 };
 
-export const NewEntryButton: FC<Props> = ({ pending, onClick }) => (
-  <button
-    type="button"
-    disabled={pending}
-    onClick={onClick}
-    className="rounded-md bg-violet-900 px-3 py-1.5 text-sm font-medium text-white hover:bg-violet-900 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-950 disabled:opacity-50"
-  >
-    New entry
-  </button>
-);
+export const NewEntryButton: FC<Props> = ({ pending, onClick }) => {
+  const disabled = pending;
+
+  return (
+    <button
+      type="button"
+      disabled={disabled}
+      onClick={onClick}
+      className={join([
+        disabled ? "cursor-not-allowed" : "cursor-pointer",
+        "rounded-md",
+        "bg-violet-900 hover:bg-violet-800 transition-colors duration-[300ms] ease-out focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-950 disabled:opacity-50",
+        "px-3 py-1.5",
+        "text-sm font-medium text-white",
+      ])}
+    >
+      New entry
+    </button>
+  );
+};
